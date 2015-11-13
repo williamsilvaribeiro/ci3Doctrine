08092a2e8d33cadfc8ebc0817bbee1d1bbba1541
William Ribeiro <williamsilva.ribeiro@gmail.com>
2015-11-13 16:18:32 -0200

    exemplo funcionando de um site

--- application/third_party/DoctrineORM-2.2.2/libraries/Doctrine.php
+++ application/third_party/DoctrineORM-2.2.2/libraries/Doctrine.php
@@ -1,23 +1,21 @@
 <?php
 
-use Doctrine\Common\ClassLoader,
-    Doctrine\ORM\Configuration,
-    Doctrine\ORM\EntityManager,
-    Doctrine\Common\Cache\ArrayCache,
-    Doctrine\DBAL\Logging\EchoSQLLogger,
-    Symfony\Component\Console;
-use Doctrine\ORM\Tools\Setup;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\ClassLoader;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Console;
+use Doctrine\ORM\Tools\EntityGenerator;
+use Doctrine\ORM\Tools\EntityRepositoryGenerator;
 
 /**
  * @property \Doctrine\ORM\EntityManager $em Gerenciador de Entidade
  */
-class Doctrine
-{
+class Doctrine {
 
     public $em = '';
 
-    public function __construct()
-    {
+    public function __construct() {
         // Set up class loading. You could use different autoloaders, provided by your favorite framework,
         // if you want to.
         require_once APPPATH . 'third_party/DoctrineORM-2.2.2/libraries/Doctrine/Common/ClassLoader.php';
@@ -27,22 +25,24 @@ class Doctrine
 
         $doctrineClassLoader = new ClassLoader('Doctrine', APPPATH . 'third_party/DoctrineORM-2.2.2/libraries');
         $doctrineClassLoader->register();
-        $proxiesClassLoader  = new ClassLoader('Proxies', APPPATH . 'models/proxies');
+        $proxiesClassLoader = new ClassLoader('Proxies', APPPATH . 'third_party/DoctrineORM-2.2.2/libraries');
         $proxiesClassLoader->register();
 
         // Set up caches
-        $config     = new Configuration;
-        $cache      = new ArrayCache;
+        $config = new Configuration;
+        $cache = new ArrayCache;
         $config->setMetadataCacheImpl($cache);
-        $driverImpl = $config->newDefaultAnnotationDriver(array(APPPATH . 'models/Entities'));
+        $driverImpl = $config->newDefaultAnnotationDriver(array(APPPATH . 'third_party/DoctrineORM-2.2.2/libraries'));
         $config->setMetadataDriverImpl($driverImpl);
         $config->setQueryCacheImpl($cache);
 
         $config->setQueryCacheImpl($cache);
 
         // Proxy configuration
-        $config->setProxyDir(APPPATH . '/models/proxies');
+        $config->setProxyDir(APPPATH . 'third_party/DoctrineORM-2.2.2/libraries/Proxies');
         $config->setProxyNamespace('Proxies');
+
+
 
         // Set up logger
         #$logger = new EchoSQLLogger;
@@ -50,16 +50,19 @@ class Doctrine
 
         $config->setAutoGenerateProxyClasses(TRUE);
 
+       // \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(array(__DIR__."/models/Entities"), true, null, null, false);
+
+
         require APPPATH . 'config/database.php';
 
         // Database connection information
         $connectionOptions = array(
-            'driver'   => 'pdo_mysql',
-            'user'     => $db['default']['username'],
+            'driver' => 'pdo_mysql',
+            'user' => $db['default']['username'],
             'password' => $db['default']['password'],
-            'host'     => $db['default']['hostname'],
-            'dbname'   => $db['default']['database'],
-            'charset'  => 'utf8',
+            'host' => $db['default']['hostname'],
+            'dbname' => $db['default']['database'],
+            'charset' => 'utf8',
             'driverOptions' => array(1002 => 'SET NAMES utf8')
         );
 
@@ -71,6 +74,47 @@ class Doctrine
 
         // Create EntityManager
         $this->em = EntityManager::create($connectionOptions, $config);
+
+        // fetch metadata
+        $driver = new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
+            $this->em->getConnection()->getSchemaManager()
+        );
+
+        $this->em->getConfiguration()->setMetadataDriverImpl($driver);
+        $cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory();
+        $cmf->setEntityManager($this->em);
+
+        $classes = $driver->getAllClassNames();
+
+        $metadata = array();
+        foreach ($classes as &$class) {
+            $metadata[] = $cmf->getMetadataFor($class);
+        }
+
+        //Cria as entidades através do Metadata Driver Impl setado
+        $generator = new EntityGenerator();
+        $generator->setRegenerateEntityIfExists(true);
+        $generator->setGenerateStubMethods(true);
+        $generator->setGenerateAnnotations(true);
+        $generator->generate($metadata, __DIR__ . '\Entities');
+
+        //Cria as classes Repository
+        foreach ($classes as &$classe) {
+            $repository = new EntityRepositoryGenerator();
+            $repository->writeEntityRepositoryClass('Repository\\' . $classe, __DIR__);
+        }
+
+        //Carrega todas as entities criadas
+        $dir = glob(__DIR__ .'\Entities/*.php');
+        foreach($dir as &$classFilename) {
+            require_once $classFilename;
+        }
+
+        //Gera as classes Proxies
+        $mt = $this->em->getMetadataFactory();
+        $this->em->getProxyFactory()->generateProxyClasses($mt->getAllMetadata());
+
+
     }
 
 }
